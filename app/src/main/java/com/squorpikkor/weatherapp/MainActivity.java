0b1bf869ecb728669851eb6afbf4061d2f081d8d
VadimSquorpikkor <uniqueehub1024>
2021-02-06 17:46:23 +0300

    работает

--- app/src/main/java/com/squorpikkor/weatherapp/MainActivity.java
+++ app/src/main/java/com/squorpikkor/weatherapp/MainActivity.java
@@ -2,15 +2,104 @@ package com.squorpikkor.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "TAG";
+
+    //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    //api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}
+    //api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
+
+    String apiKey = "9a8038ffd08c553cdbeb6c5bbd8541ca";//openweathermap.org
+    public static final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&lang=ru&units=metric";
+    private TextView textViewWeather;
+    private EditText editTextCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewWeather = findViewById(R.id.text_view_weather);
+        editTextCity = findViewById(R.id.edit_text_city);
+
+        findViewById(R.id.button_show_weather).setOnClickListener(view -> getWeather());
     }
 
+    private void getWeather() {
+        String city = editTextCity.getText().toString().trim();
+        if (!city.isEmpty()) {
+            DownloadWeatherTask task = new DownloadWeatherTask();
+            String url = String.format(WEATHER_URL, city, apiKey);
+            task.execute(url);
+        }
+    }
+
+    private class DownloadWeatherTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            StringBuilder result = new StringBuilder();
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String line = reader.readLine();
+                while (line != null) {
+                    result.append(line);
+                    line = reader.readLine();
+                }
+                return result.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return result.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String city = jsonObject.getString("name");
+                String temp = jsonObject.getJSONObject("main").getString("temp");
+                String tempFeels = jsonObject.getJSONObject("main").getString("feels_like");
+                String description = jsonObject.getJSONArray("weather").getJSONObject(0).getString("description");
+                String weather = String.format("%s\nТемпература: %s\nощущается как: %s\nна улице: %s", city, temp, tempFeels, description);
+                textViewWeather.setText(weather);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 
 }
